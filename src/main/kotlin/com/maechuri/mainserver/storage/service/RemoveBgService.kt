4de61be47bc3d9b5859fa23845c209fe0f759112
Yunseong <dev.yunseong@gmail.com>
2026-03-01 11:39:40 +0900

    feat: enhance background removal service with error handling and logging

--- src/main/kotlin/com/maechuri/mainserver/storage/service/RemoveBgService.kt
+++ src/main/kotlin/com/maechuri/mainserver/storage/service/RemoveBgService.kt
@@ -1,15 +1,28 @@
 package com.maechuri.mainserver.storage.service
 
 import com.maechuri.mainserver.storage.client.RemoveBgClient
+import mu.KotlinLogging
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClientResponseException
+
+private val logger = KotlinLogging.logger {}
 
 @Service
 @Primary
 class RemoveBgService(
     private val removeBgClient: RemoveBgClient
+
 ) : BackgroundRemovalService {
     override suspend fun removeBackground(imageBytes: ByteArray): ByteArray {
-        return removeBgClient.removeBackground(imageBytes)
+        try {
+            return removeBgClient.removeBackground(imageBytes)
+        } catch (e: WebClientResponseException) {
+            val errorBody = e.responseBodyAsString
+            logger.error("에러 발생! 상태코드: ${e.statusCode}, 메시지: $errorBody")
+            return imageBytes
+        } catch (e: Exception) {
+            throw e
+        }
     }
 }
